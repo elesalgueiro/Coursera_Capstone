7bf59693991a4a32b61a0373d625243bd84ac0ea
elesalgueiro <67014486+elesalgueiro@users.noreply.github.com>
2020-06-17 19:49:41 +0200

    Add files via upload

---
+++ DS Capston Project.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# This is my Jupyter Notebook and is going to be used for the Data Scince capstone project of the coursera and IBM course
+
+# In[2]:
+
+
+import pandas as pd
+
+
+# In[4]:
+
+
+import numpy as np
+
+
+# In[1]:
+
+
+print('Hello Capstone Project course!')
+
+
+# In[ ]:
+
+
+
+
